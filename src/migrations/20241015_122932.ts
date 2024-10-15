1fd66d31fff6ce31f893f9938787fa6d2328593b
Oliver Studer <oliver.studer@tegonal.com>
2024-10-15 14:29:58 +0200

    add missing migration

---
+++ src/migrations/20241015_122932.ts
@@ -0,0 +1,24 @@
+import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-postgres';
+
+export async function up({ payload, req }: MigrateUpArgs): Promise<void> {
+  await payload.db.drizzle.execute(sql`
+   CREATE TABLE IF NOT EXISTS "cv_skill_highlights_locales" (
+  	"description" varchar,
+  	"id" serial PRIMARY KEY NOT NULL,
+  	"_locale" "_locales" NOT NULL,
+  	"_parent_id" varchar NOT NULL,
+  	CONSTRAINT "cv_skill_highlights_locales_locale_parent_id_unique" UNIQUE("_locale","_parent_id")
+  );
+  
+  DO $$ BEGIN
+   ALTER TABLE "cv_skill_highlights_locales" ADD CONSTRAINT "cv_skill_highlights_locales_parent_id_fk" FOREIGN KEY ("_parent_id") REFERENCES "public"."cv_skill_highlights"("id") ON DELETE cascade ON UPDATE no action;
+  EXCEPTION
+   WHEN duplicate_object THEN null;
+  END $$;
+  `);
+}
+
+export async function down({ payload, req }: MigrateDownArgs): Promise<void> {
+  await payload.db.drizzle.execute(sql`
+   DROP TABLE "cv_skill_highlights_locales";`);
+}
--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,6 +1,7 @@
 import * as migration_20241008_105308 from './20241008_105308';
 import * as migration_20241008_200824 from './20241008_200824';
 import * as migration_20241009_083126 from './20241009_083126';
+import * as migration_20241015_122932 from './20241015_122932';
 
 export const migrations = [
   {
@@ -17,5 +18,10 @@ export const migrations = [
     up: migration_20241009_083126.up,
     down: migration_20241009_083126.down,
     name: '20241009_083126',
+  },
+  {
+    up: migration_20241015_122932.up,
+    down: migration_20241015_122932.down,
+    name: '20241015_122932',
   },
 ];
